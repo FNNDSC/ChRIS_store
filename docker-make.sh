4983bf105c353518ef9439da8b098cac96884404
Jorge <jbernal0019@yahoo.es>
2018-02-15 16:47:47 -0500

    Enable application of migrations and default API users creation

--- docker-make.sh
+++ docker-make.sh
@@ -24,7 +24,7 @@ docker-compose exec chris_store_dev_db mysql -uroot -prootp -e 'GRANT ALL PRIVIL
 windowBottom
 
 title -d 1 "Applying migrations..."
-#docker-compose exec chris_store_dev python manage.py migrate
+docker-compose exec chris_store_dev python manage.py migrate
 windowBottom
 
 title -d 1 "Running Django Unit tests..."
@@ -32,17 +32,17 @@ title -d 1 "Running Django Unit tests..."
 windowBottom
 
 title -d 1 "Creating two ChRIS store API users"
-#echo ""
-#echo "Setting superuser chris:chris1234 ..."
-#docker-compose exec chris_dev /bin/bash -c 'python manage.py createsuperuser --noinput --username chris --email dev@babymri.org 2> /dev/null;'
-#docker-compose exec chris_dev /bin/bash -c \
-#'python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username=\"chris\"); user.set_password(\"chris1234\"); user.save()"'
-#echo ""
-#echo "Setting normal user cube:cube1234 ..."
-#docker-compose exec chris_dev /bin/bash -c 'python manage.py createsuperuser --noinput --username cube --email dev@babymri.org 2> /dev/null;'
-#docker-compose exec chris_dev /bin/bash -c \
-#'python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username=\"cube\"); user.set_password(\"cube1234\"); user.save()"'
-#echo ""
+echo ""
+echo "Setting superuser chris:chris1234 ..."
+docker-compose exec chris_store_dev /bin/bash -c 'python manage.py createsuperuser --noinput --username chris --email dev@babymri.org 2> /dev/null;'
+docker-compose exec chris_store_dev /bin/bash -c \
+'python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username=\"chris\"); user.set_password(\"chris1234\"); user.save()"'
+echo ""
+echo "Setting normal user cube:cube1234 ..."
+docker-compose exec chris_store_dev /bin/bash -c 'python manage.py createsuperuser --noinput --username cube --email cube@babymri.org 2> /dev/null;'
+docker-compose exec chris_store_dev /bin/bash -c \
+'python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username=\"cube\"); user.set_password(\"cube1234\"); user.save()"'
+echo ""
 windowBottom
 
 title -d 1 "Restarting CUBE's Django development server in interactive mode..."
