94a2c194da68e1240ed09011426f680e3879e85e
Jorge <jbernal0019@yahoo.es>
2018-02-15 16:48:29 -0500

    Now also destroys swift storage volume

--- docker-destroy.sh
+++ docker-destroy.sh
@@ -16,6 +16,7 @@ windowBottom
 title -d 1 "Destroying persistent volumes..."
     a_PVOLS=(
         "chrisstore_chris_store_dev_db_data"
+        "chrisstore_swift_storage"
     )
     for VOL in ${a_PVOLS[@]} ; do 
         read -p  "Do you want to remove persistent volume $VOL? " -n 1 -r
