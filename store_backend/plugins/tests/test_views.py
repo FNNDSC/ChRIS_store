3b2a1a890e06d487c6effb83b514f5e921e61847
Jorge <jbernal0019@yahoo.es>
2018-03-01 16:01:19 -0500

    Add tests for the plugin parameters views

--- store_backend/plugins/tests/test_views.py
+++ store_backend/plugins/tests/test_views.py
@@ -6,13 +6,11 @@ from unittest import mock
 from django.test import TestCase, tag
 from django.core.urlresolvers import reverse
 from django.contrib.auth.models import User
-from django.conf import settings
 
 from rest_framework import status
 
 from plugins.models import Plugin, PluginParameter
 from plugins.services.manager import PluginManager
-from plugins import views
 
 class ViewTests(TestCase):
     
@@ -122,34 +120,6 @@ class PluginDetailViewTests(ViewTests):
         response = self.client.get(self.read_update_url)
         self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_plugin_update_success(self):
-
-        plugin_parameters = [{'name': 'dir', 'type': str.__name__,
-                                      'optional': False, 'flag':'--dir', 'default': '',
-                                      'help': 'test plugin'}]
-        plg_repr = {}
-        plg_repr['name'] = self.plugin_name
-        plg_repr['type'] = 'fs'
-        plg_repr['authors'] = 'DEV FNNDSC'
-        plg_repr['title'] = 'Dir plugin'
-        plg_repr['description'] = 'Dir test plugin'
-        plg_repr['license'] = 'MIT'
-        plg_repr['version'] = 'v0.1'
-        plg_repr['execshell'] = 'python3'
-        plg_repr['selfpath'] = '/usr/src/simplefsapp'
-        plg_repr['selfexec'] = 'simplefsapp.py'
-        plg_repr['parameters'] = plugin_parameters
-
-        self.client.login(username=self.username, password=self.password)
-        with io.StringIO(json.dumps(plg_repr)) as f:
-            put = {"descriptor_file": f, "public_repo": "http://localhost",
-                   "dock_image": "fnndsc-pl-testplugin"}
-        response = self.client.put(self.read_update_url, data=put,
-                                   content_type='multipart/form-data')
-        import pdb; pdb.set_trace()
-        self.assertContains(response, "fnndsc-pl-testplugin")
-        self.assertContains(response, self.plugin_name)
-
     def test_plugin_update_failure_unauthenticated(self):
         response = self.client.put(self.read_update_url, data={},
                                    content_type=self.content_type)
@@ -157,9 +127,88 @@ class PluginDetailViewTests(ViewTests):
 
     def test_plugin_update_failure_access_denied(self):
         # create another chris store user
-        user = User.objects.create_user(username='another', email='another@babymri.org',
-                                        password='anotherpassword')
+        User.objects.create_user(username='another', email='another@babymri.org',
+                                 password='anotherpassword')
         self.client.login(username='another', password='anotherpassword')
         response = self.client.put(self.read_update_url, data={},
                                    content_type=self.content_type)
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+class PluginListQuerySearchViewTests(ViewTests):
+    """
+    Test the plugin-list-query-search view
+    """
+
+    def setUp(self):
+        super(PluginListQuerySearchViewTests, self).setUp()
+        self.list_url = reverse("plugin-list-query-search") + '?name=' + self.plugin_name
+
+    def test_plugin_list_query_search_success(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(self.list_url)
+        self.assertContains(response, self.plugin_name)
+
+    def test_plugin_list_query_search_failure_unauthenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PluginParameterListViewTests(ViewTests):
+    """
+    Test the pluginparameter-list view
+    """
+
+    def setUp(self):
+        super(PluginParameterListViewTests, self).setUp()
+        self.plugin_parameters = [{'name': 'dir', 'type': str.__name__,
+                                      'optional': False, 'flag':'--dir', 'default': '',
+                                      'help': 'test plugin'}]
+        plugin = Plugin.objects.get(name=self.plugin_name)
+        self.list_url = reverse("pluginparameter-list", kwargs={"pk": plugin.id})
+
+        # add plugin's parameters
+        PluginParameter.objects.get_or_create(
+            plugin=plugin,
+            name=self.plugin_parameters[0]['name'],
+            type=self.plugin_parameters[0]['type'],
+            optional=self.plugin_parameters[0]['optional'])
+
+    def test_plugin_parameter_list_success(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(self.list_url)
+        self.assertContains(response, self.plugin_parameters[0]['name'])
+
+    def test_plugin_parameter_list_failure_unauthenticated(self):
+        response = self.client.get(self.list_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+
+class PluginParameterDetailViewTests(ViewTests):
+    """
+    Test the pluginparameter-detail view
+    """
+
+    def setUp(self):
+        super(PluginParameterDetailViewTests, self).setUp()
+        self.plugin_parameters = [{'name': 'dir', 'type': str.__name__,
+                                      'optional': False, 'flag':'--dir', 'default': '',
+                                      'help': 'test plugin'}]
+        plugin = Plugin.objects.get(name=self.plugin_name)
+        # create a plugin parameter
+        (param, tf) = PluginParameter.objects.get_or_create(
+            plugin=plugin,
+            name=self.plugin_parameters[0]['name'],
+            type=self.plugin_parameters[0]['type'],
+            optional=self.plugin_parameters[0]['optional'])
+
+        self.read_url = reverse("pluginparameter-detail", kwargs={"pk": param.id})
+
+    def test_plugin_parameter_detail_success(self):
+        self.client.login(username=self.username, password=self.password)
+        response = self.client.get(self.read_url)
+        self.assertContains(response, self.plugin_parameters[0]['name'])
+
+    def test_plugin_parameter_detail_failure_unauthenticated(self):
+        response = self.client.get(self.read_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
