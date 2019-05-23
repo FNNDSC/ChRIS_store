20033760d7e305e5461f7942fecf87166a85cb74
Jorge <jbernal0019@yahoo.es>
2019-05-23 14:07:29 -0400

    Add pipeline manager utility module.

--- store_backend/pipelines/services/manager.py
+++ store_backend/pipelines/services/manager.py
@@ -4,7 +4,6 @@ Pipeline manager module that provides functionality to add, modify and delete pi
 
 import os
 import sys
-import json
 from argparse import ArgumentParser
 
 if "DJANGO_SETTINGS_MODULE" not in os.environ:
@@ -14,10 +13,7 @@ if "DJANGO_SETTINGS_MODULE" not in os.environ:
     import django
     django.setup()
 
-from django.core.files.base import ContentFile
 from django.contrib.auth.models import User
-from plugins.models import Plugin
-from plugins.serializers import PluginSerializer
 from pipelines.models import Pipeline
 from pipelines.serializers import PipelineSerializer
 
@@ -32,21 +28,27 @@ class PipelineManager(object):
 
         # create the parser for the "add" command
         parser_add = subparsers.add_parser('add', help='Add a new pipeline')
-        parser_add.add_argument('name', help="Plugin's name")
-        parser_add.add_argument('owner', help="Plugin's owner username")
-        parser_add.add_argument('publicrepo', help="Plugin's public repo url")
-        parser_add.add_argument('dockerimage', help="Plugin's docker image name")
-        group = parser_add.add_mutually_exclusive_group()
-        group.add_argument("--descriptorfile", dest='descriptorfile', type=str,
-                           help="A json descriptor file with the plugin representation")
-        group.add_argument("--descriptorstring", dest='descriptorstring', type=str,
-                           help="A json string with the plugin representation")
+        parser_add.add_argument('name', help="Pipeline's name")
+        parser_add.add_argument('owner', help="Pipeline's owner username")
+        parser_add.add_argument('plugintree',
+                           help="A json string with the plugin tree for the pipeline")
+        parser_add.add_argument('--authors', help="Pipeline's authors string")
+        parser_add.add_argument('--category', help="Pipeline's category")
+        parser_add.add_argument('--description', help="Pipeline's description")
+        parser_add.add_argument('--unlock', action='store_true',
+                                help="Unlock pipeline to make it immutable and visible "
+                                     "to other users")
 
         # create the parser for the "modify" command
         parser_modify = subparsers.add_parser('modify', help='Modify existing pipeline')
         parser_modify.add_argument('id', type=int, help="Plugin's id")
-        parser_modify.add_argument('publicrepo', help="Plugin's new public repo url")
-        parser_modify.add_argument('--newowner', help="Plugin's new owner username")
+        parser_modify.add_argument('--name', help="Pipeline's name")
+        parser_modify.add_argument('--authors', help="Pipeline's authors string")
+        parser_modify.add_argument('--category', help="Pipeline's category")
+        parser_modify.add_argument('--description', help="Pipeline's description")
+        parser_modify.add_argument('--unlock', action='store_true',
+                                help="Unlock pipeline to make it immutable and visible "
+                                     "to other users")
 
         # create the parser for the "remove" command
         parser_remove = subparsers.add_parser('remove', help='Remove an existing pipeline')
@@ -56,47 +58,54 @@ class PipelineManager(object):
 
     def add_pipeline(self, args):
         """
-        Register/add a new plugin to the system.
+        Add a new pipeline to the system.
         """
-        df = self.get_plugin_descriptor_file(args)
-        data = {'name': args.name, 'public_repo': args.publicrepo, 'version': 'nullnull',
-                'dock_image': args.dockerimage, 'descriptor_file': df}
-        plg_serializer = PluginSerializer(data=data)
-        plg_serializer.is_valid(raise_exception=True)
+        data = {'name': args.name, 'plugin_tree': args.plugintree}
+        if args.authors:
+            data['authors'] = args.authors
+        if args.category:
+            data['category'] = args.category
+        if args.description:
+            data['description'] = args.description
+        if args.unlock:
+            data['locked'] = False
+        pipeline_serializer = PipelineSerializer(data=data)
+        pipeline_serializer.is_valid(raise_exception=True)
         owner = User.objects.get(username=args.owner)
-        plg_serializer.save(owner=[owner])
+        pipeline_serializer.save(owner=owner)
 
     def modify_pipeline(self, args):
         """
-        Modify an existing/registered plugin.
+        Modify an existing pipeline.
         """
-        plugin = self.get_plugin(args.id)
-        data = {'public_repo': args.publicrepo, 'dock_image': plugin.dock_image,
-                'name': plugin.name, 'descriptor_file': plugin.descriptor_file,
-                'version': plugin.version}
-        plg_serializer = PluginSerializer(plugin, data=data)
-        plg_serializer.is_valid(raise_exception=True)
-        if args.newowner:
-            new_owner = plg_serializer.validate_new_owner(args.newowner)
-            plugin.add_owner(new_owner)
-        plg_serializer.save()
+        pipeline = self.get_pipeline(args.id)
+        data = {}
+        if args.name:
+            data['name'] = args.name
+        if args.authors:
+            data['authors'] = args.authors
+        if args.category:
+            data['category'] = args.category
+        if args.description:
+            data['description'] = args.description
+        if args.unlock:
+            data['locked'] = False
+        pipeline_serializer = PipelineSerializer(pipeline, data=data)
+        pipeline_serializer.is_valid(raise_exception=True)
+        pipeline_serializer.save()
 
     def remove_pipeline(self, args):
         """
-        Remove an existing/registered plugin from the system.
+        Remove an existing pipeline from the system.
         """
-        plugin = self.get_plugin(args.id)
-        plugin.delete()
+        pipeline = self.get_pipeline(args.id)
+        pipeline.delete()
 
     def run(self, args=None):
         """
         Parse the arguments passed to the manager and perform the appropriate action.
         """
         options = self.parser.parse_args(args)
-        if (options.subparser_name == 'add') and (not options.descriptorfile) and (
-                not options.descriptorstring):
-                self.parser.error("Either --descriptorFile or --descriptorString must be "
-                                  "specified")
         if options.subparser_name == 'add':
             self.add_pipeline(options)
         elif options.subparser_name == 'modify':
@@ -105,28 +114,15 @@ class PipelineManager(object):
             self.remove_pipeline(options)
 
     @staticmethod
-    def get_plugin(id):
+    def get_pipeline(id):
         """
-        Get an existing plugin.
+        Get an existing pipeline.
         """
         try:
-            plugin = Plugin.objects.get(pk=id)
-        except Plugin.DoesNotExist:
-            raise NameError("Couldn't find plugin with id '%s' in the system" % id)
-        return plugin
-
-    @staticmethod
-    def get_plugin_descriptor_file(args):
-        """
-        Get the plugin descriptor file from the arguments.
-        """
-        if args.descriptorfile:
-            f = args.descriptorfile
-        else:
-            app_repr = json.loads(args.descriptorstring)
-            f = ContentFile(json.dumps(app_repr).encode())
-            f.name = args.name + '.json'
-        return f
+            pipeline = Pipeline.objects.get(pk=id)
+        except Pipeline.DoesNotExist:
+            raise NameError("Couldn't find pipeline with id '%s' in the system" % id)
+        return pipeline
 
 
 
