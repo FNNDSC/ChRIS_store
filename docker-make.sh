7ba266895ba7f4815245770301a9ade583aa5d46
Jorge <jbernal0019@yahoo.es>
2019-05-29 13:39:20 -0400

    Improve decoration of the script's prompt output.

--- docker-make.sh
+++ docker-make.sh
@@ -36,19 +36,19 @@ windowBottom
 
 title -d 1 "Creating two ChRIS store API users"
 echo ""
-echo "Setting superuser chris:chris1234 ..."
+echo "Setting superuser chris:chris1234..."
 docker-compose exec chris_store_dev /bin/bash -c 'python manage.py createsuperuser --noinput --username chris --email dev@babymri.org 2> /dev/null;'
 docker-compose exec chris_store_dev /bin/bash -c \
 'python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username=\"chris\"); user.set_password(\"chris1234\"); user.save()"'
 echo ""
-echo "Setting normal user cubeadmin:cubeadmin1234 ..."
+echo "Setting normal user cubeadmin:cubeadmin1234..."
 docker-compose exec chris_store_dev /bin/bash -c 'python manage.py createsuperuser --noinput --username cubeadmin --email cubeadmin@babymri.org 2> /dev/null;'
 docker-compose exec chris_store_dev /bin/bash -c \
 'python manage.py shell -c "from django.contrib.auth.models import User; user = User.objects.get(username=\"cubeadmin\"); user.set_password(\"cubeadmin1234\"); user.save()"'
 echo ""
 windowBottom
 
-title -d 1 "Automatically uploading some plugins to the ChRIS STORE ..."
+title -d 1 "Automatically uploading some plugins to the ChRIS STORE..."
 # Declare an array variable for the list of plugin names to be automatically registered
 # Add a new plugin name to the list if you want it to be automatically registered
 declare -a plugins=( "simplefsapp"
@@ -62,7 +62,7 @@ declare -a plugins=( "simplefsapp"
 declare -i i=1
 declare -i STEP=7
 for plugin in "${plugins[@]}"; do
-    echo "${STEP}.$i: Uploading $plugin representation to the ChRIS store ..."
+    echo "${STEP}.$i: Uploading $plugin representation to the ChRIS store..."
     PLUGIN_DOCK="fnndsc/pl-${plugin}"
     PLUGIN_REP=$(docker run --rm "$PLUGIN_DOCK" "${plugin}.py" --json 2> /dev/null;)
     docker-compose exec chris_store_dev python plugins/services/manager.py add "${plugin}" cubeadmin https://github.com/FNNDSC "$PLUGIN_DOCK" --descriptorstring "$PLUGIN_REP"
@@ -74,7 +74,6 @@ title -d 1 "Automatically creating a locked pipeline in the ChRIS STORE" "(mutab
 S3_PLUGIN_VER=$(docker run --rm fnndsc/pl-s3retrieve s3retrieve.py --version)
 SIMPLEDS_PLUGIN_VER=$(docker run --rm fnndsc/pl-simpledsapp simpledsapp.py --version)
 PIPELINE_NAME="s3retrieve_v${S3_PLUGIN_VER}-simpledsapp_v${SIMPLEDS_PLUGIN_VER}"
-echo "Creating pipeline named '$PIPELINE_NAME' ..."
 
 STR1='[{"plugin_name": "s3retrieve", "plugin_version": "'
 STR2='", "plugin_parameter_defaults": [{"name": "awssecretkey", "default": "somekey"},{"name": "awskeyid", "default": "somekeyid"}], "previous_index": null},
@@ -83,11 +82,11 @@ STR3='", "previous_index": 0}]'
 PLUGIN_TREE=${STR1}${S3_PLUGIN_VER}${STR2}${SIMPLEDS_PLUGIN_VER}${STR3}
 
 docker-compose exec chris_store_dev python pipelines/services/manager.py add "${PIPELINE_NAME}" cubeadmin "${PLUGIN_TREE}"
+echo "Created pipeline named '$PIPELINE_NAME'"
 windowBottom
 
 title -d 1 "Automatically creating an unlocked pipeline in the ChRIS STORE" "(unmutable and available to all users)"
 PIPELINE_NAME="simpledsapp_v${SIMPLEDS_PLUGIN_VER}-simpledsapp_v${SIMPLEDS_PLUGIN_VER}-simpledsapp_v${SIMPLEDS_PLUGIN_VER}"
-echo "Creating pipeline named '$PIPELINE_NAME' ..."
 
 STR4='[{"plugin_name": "simpledsapp", "plugin_version": "'
 STR5='", "previous_index": null},{"plugin_name": "simpledsapp", "plugin_version": "'
@@ -96,6 +95,7 @@ STR7='", "previous_index": 0}]'
 PLUGIN_TREE=${STR4}${SIMPLEDS_PLUGIN_VER}${STR5}${SIMPLEDS_PLUGIN_VER}${STR6}${SIMPLEDS_PLUGIN_VER}${STR7}
 
 docker-compose exec chris_store_dev python pipelines/services/manager.py add "${PIPELINE_NAME}" cubeadmin "${PLUGIN_TREE}" --unlock
+echo "Created pipeline named '$PIPELINE_NAME'"
 windowBottom
 
 title -d 1 "Restarting ChRIS store's Django development server" "in interactive mode..."
