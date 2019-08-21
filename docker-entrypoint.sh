60b184154cb253f0087da0368f0be90e49d4d5fe
Jorge <jbernal0019@yahoo.es>
2019-08-21 17:11:49 -0400

    Remove '--server-name localhost' from mod_wsgi-express command to allow access from any network interface.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,7 +4,7 @@
 python check_db_connection.py -u root -p rootp --host chris_store_dev_db --max-attempts 30
 python manage.py migrate
 #python manage.py collectstatic
-mod_wsgi-express start-server config/wsgi.py --host 0.0.0.0 --port 8010 --processes 8 --server-name localhost --server-root ~/mod_wsgi-0.0.0.0:8010
+mod_wsgi-express start-server config/wsgi.py --host 0.0.0.0 --port 8010 --processes 8 --server-root ~/mod_wsgi-0.0.0.0:8010
 #mod_wsgi-express setup-server config/wsgi.py --host 0.0.0.0 --port 8010 --processes 8 --server-name localhost --server-root ~/mod_wsgi-0.0.0.0:8010
 #to start daemon:
 #~/mod_wsgi-0.0.0.0:8010/apachectl start
