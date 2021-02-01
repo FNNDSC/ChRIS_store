5580e8257f6fe828d56e46d0f803c09d9f28cce9
Jennings Zhang <jenni_zh@protonmail.com>
2021-02-01 17:03:44 -0500

    Support no parallel

--- utils/scripts/upload_all_from_github.sh
+++ utils/scripts/upload_all_from_github.sh
@@ -16,9 +16,18 @@ org='FNNDSC'
 search="topic:chris-app+org:$org"
 
 source_dir=$(dirname "$(readlink -f "$0")")
-[ -n "$1" ] && jobs="-j $1"
 
-curl -s "https://api.github.com/search/repositories?per_page=100&q=$search" \
-  -H 'accept:application/vnd.github.v3+json' \
-  | jq -r '.items[] | .full_name' \
-  | parallel $jobs $source_dir/upload_one_from_github.sh {}
+list=$(
+  curl -s "https://api.github.com/search/repositories?per_page=100&q=$search" \
+    -H 'accept:application/vnd.github.v3+json' \
+    | jq -r '.items[] | .full_name' 
+)
+
+
+if [ -n "$1" ]; then
+  parallel -j $1 $source_dir/upload_one_from_github.sh {} <<< $list
+else
+  for repo in $list; do
+    $source_dir/upload_one_from_github.sh $repo
+  done
+fi
