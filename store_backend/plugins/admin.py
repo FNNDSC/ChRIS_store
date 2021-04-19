fc42c305c1ffdd21b7b14c7317e5964f26777442
EUGINELETHAL <ochungeugine@gmail.com>
2021-04-19 22:22:53 +0300

    registered admin models

--- store_backend/plugins/admin.py
+++ store_backend/plugins/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import *
+from .models import PluginMeta,Plugin,PluginParameter
 
 admin.site.register(PluginMeta)
 admin.site.register(Plugin)
+admin.site.register(PluginParameter)
